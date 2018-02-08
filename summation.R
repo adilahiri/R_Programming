4ea3e363fa1804db62c79576e2715ce74f4fbb1b
adilahiri <lahiri@aggienetwork.com>
2018-02-08 13:56:40 -0600

    summation

---
+++ summation.R
@@ -0,0 +1,5 @@
+# The complete code is given. You can just review and submit!
+nums <- read.table("/dev/stdin", sep=" ");
+nums <- as.list(as.data.frame(t(nums)))
+write.table(as.numeric(nums[1])+as.numeric(nums[2]), sep = "", append=T, row.names = F, col.names = F)
+print(num[1]+num[2])
